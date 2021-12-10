5fde812b241946d1c9372ce29f7b5b1a995b3d97
Lucasinho11 <lucas.lubasinski11@gmail.com>
2021-12-10 14:24:06 +0100

    second screen

--- Screens/Test.js
+++ Screens/Test.js
@@ -2,16 +2,17 @@
 import React, { Component } from 'react';
 import { View, ScrollView, Text, ImageBackground, StyleSheet } from 'react-native'
 import Header from '../Components/Header';
+import Promotions from '../Components/Promotions';
+import Betters from '../Components/Betters';
+import Exclus from '../Components/Exclus';
 class Test extends Component {
   render() {
     return (
       <View>
         <Header/>
-        <Text>test</Text>
-        <Text>test</Text>
-        <Text>test</Text>
-        <Text>test</Text>
-        <Text>test</Text>
+            <View style={{paddingBottom: 10}}>
+              <Betters/>
+            </View>
       </View>
     )
   
